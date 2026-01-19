f98d591a39d882f0a783ae77908b66b1b9681926
ibrahim babangida <babzibrahim@gmail.com>
2026-01-19 13:08:11 +0100

    refactor: restructure book API with FastAPI and add book data

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,38 +1,21 @@
-from fastapi import FastAPI, status
+from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
-from pydantic import BaseModel
 from typing import List
-app  = FastAPI()
+from src.books.book_data import books
+from src.books.schemas import Book, BookUpdateModel
+book_router =APIRouter()
 
-class Book(BaseModel):
-        id: int
-        title: str
-        author: str
-        publisher: str
-        published_date: str
-        page_count: int
-        language: str
-
-class BookUpdateModel(BaseModel):
-        
-        title: str
-        author: str
-        publisher: str
-   
-        page_count: int
-        language: str    
-
-@app.get('/books', response_model= List[Book])
+@book_router.get('/', response_model= List[Book])
 async def get_all_books():
     return books
 
-@app.post("/books", status_code=status.HTTP_201_CREATED)
+@book_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_a_book(book_data:Book) -> dict:
     new_book = book_data.model_dump()
     books.append(new_book)
     return new_book
 
-@app.get("/book/{book_id}")
+@book_router.get("/{book_id}")
 async def get_book(book_id:int) -> dict:
     for book in books:
          if book["id"] == book_id:
@@ -42,7 +25,7 @@ async def get_book(book_id:int) -> dict:
               status_code=status.HTTP_404_NOT_FOUND,
                 detail="Book is not found")
 
-@app.patch("/book/{book_id}")
+@book_router.patch("/{book_id}")
 async def update_book(book_id:int, book_update_data: BookUpdateModel) -> dict:
     for book in books:
          if book['id'] == book_id:
@@ -53,7 +36,7 @@ async def update_book(book_id:int, book_update_data: BookUpdateModel) -> dict:
               return book
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book is not found")
 
-@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+@book_router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id:int) :
     for book in books:
          if book["id"] == book_id:
