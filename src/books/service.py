02bda2e3ce5fbc06a473df3ed6a03d2e67a4e41a
ibrahim babangida <babzibrahim@gmail.com>
2026-01-26 21:27:45 +0100

    service commit

--- src/books/service.py
+++ src/books/service.py
@@ -17,10 +17,27 @@ class BookService:
         return result.first
 
     async def create_book(self,book_data:BookCreateModel, session:AsyncSession):
-        pass             
+        book_data_dict = book_data.model_dump()     
+        new_book = Book(
+            **book_data_dict
+        )      
+
+        session.add(new_book)
+
+        await session.commit()
+        return new_book
 
     async def update_book(self,book_uid:str, update_data:BookUpdateModel, session:AsyncSession):
-        pass 
+        book_to_update = self.get_book(book_uid, session)
+
+        update_data_dict = update_data.model_dump() 
+
+        for k, v in update_data_dict.items():
+            setattr(book_to_update, k,v)
+
+        await session.commit()
+
+        return book_to_update    
 
     async def delete_book(self,book_uid:str, session:AsyncSession):
         pass 
