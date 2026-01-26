224d3e491b0af394e0ed050dd8d42e2ad831f5f3
ibrahim babangida <babzibrahim@gmail.com>
2026-01-26 21:27:33 +0100

    scheme commit

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,13 +1,26 @@
+from datetime import datetime
+import uuid
 from pydantic import BaseModel
 
 class Book(BaseModel):
-        id: int
+        id: uuid.UUID
         title: str
         author: str
         publisher: str
         published_date: str
         page_count: int
         language: str
+        created_at:datetime
+        update_at:datetime
+
+class BookCreateModel(BaseModel):
+        title: str
+        author: str
+        publisher: str
+        published_date: str
+        page_count: int
+        language: str
+
 
 class BookUpdateModel(BaseModel):
         
---
+++ src/books/service.py
@@ -0,0 +1,26 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from .schemas import BookCreateModel,BookUpdateModel
+from sqlmodel import select, desc
+from .models import Book
+class BookService:
+    async def get_all_books(self, session:AsyncSession):
+        statement = select(Book).order_by(desc(Book.created_at))
+
+        result= await session.exec(statement)
+
+        return result.all()
+
+    async def get_book(self,book_uid:str, session:AsyncSession):
+        statement = select(Book).where(Book.uid == book_uid)
+        result = await session.exec(statement)
+
+        return result.first
+
+    async def create_book(self,book_data:BookCreateModel, session:AsyncSession):
+        pass             
+
+    async def update_book(self,book_uid:str, update_data:BookUpdateModel, session:AsyncSession):
+        pass 
+
+    async def delete_book(self,book_uid:str, session:AsyncSession):
+        pass 
